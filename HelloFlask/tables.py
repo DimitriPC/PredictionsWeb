17298bf801c106d0275dba6b804b2911d5919206
Dimitri Pierre-Charles <dimitripierrecharles@yahoo.fr>
2026-02-15 14:47:04 -0500

    Changed tables schema

--- HelloFlask/tables.py
+++ HelloFlask/tables.py
@@ -1,5 +1,5 @@
 from typing import List
-from sqlalchemy import ForeignKey,ForeignKeyConstraint, UniqueConstraint
+from sqlalchemy import ForeignKey,ForeignKeyConstraint, UniqueConstraint, Numeric
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from flask_sqlalchemy import SQLAlchemy
 from HelloFlask import app, db
@@ -10,8 +10,9 @@ from datetime import date, time
 class Individu(db.Model):
     __tablename__ = "individu"
 
-    nomComplet: Mapped[str] = mapped_column(primary_key=True)
-    password: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    nomComplet: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(db.String(60), nullable=False)
 
     predictions: Mapped[List["Prediction"]] = relationship(back_populates="individu", 
                                                            cascade="all, delete-orphan",
@@ -20,48 +21,47 @@ class Individu(db.Model):
 class Match(db.Model):
     __tablename__ = "match"
 
-    equipe1: Mapped[str] = mapped_column(primary_key=True)
-    equipe2: Mapped[str] = mapped_column(primary_key=True)
-    stadeCompet: Mapped[str] = mapped_column(primary_key=True)
-    dateMatch: Mapped[date]
-    heureMatch: Mapped[time]
-    resultat: Mapped[str]
-    scoreEquipe1: Mapped[int]
-    scoreEquipe2: Mapped[int]
-    coteEquipe1: Mapped[int]
-    coteEquipe2: Mapped[int]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    equipe1: Mapped[str] = mapped_column(nullable=False)
+    equipe2: Mapped[str] = mapped_column(nullable=False)
+    stadeCompet: Mapped[str] = mapped_column(nullable=False)
+    dateMatch: Mapped[date | None]
+    heureMatch: Mapped[time | None]
+    resultat: Mapped[str | None]
+    scoreEquipe1: Mapped[int | None]
+    scoreEquipe2: Mapped[int | None]
+    coteEquipe1: Mapped[int | None] = mapped_column(Numeric(6,2))
+    coteEquipe2: Mapped[int | None] = mapped_column(Numeric(6,2))
 
     predictions: Mapped[List["Prediction"]] = relationship(back_populates="game", 
                                                            cascade="all, delete-orphan", 
                                                            passive_deletes=True)
 
+    __table_args__ = (
+        UniqueConstraint("equipe1", "equipe2", "stadeCompet"),
+    )
+
 class Prediction(db.Model):
     __tablename__ = "prediction"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    individu_nomComplet: Mapped[str] = mapped_column(ForeignKey("individu.nomComplet", 
-                                                                ondelete="CASCADE", onupdate="CASCADE"), 
+    individu_id: Mapped[str] = mapped_column(ForeignKey("individu.id", 
+                                                                ondelete="CASCADE"), 
                                                      nullable=False)
-    equipe1: Mapped[str] = mapped_column(nullable=False)
-    equipe2: Mapped[str] = mapped_column(nullable=False)
-    stadeCompet: Mapped[str] = mapped_column(nullable=False)
+    idMatch: Mapped[int] = mapped_column(ForeignKey("match.id", 
+                                                    ondelete="CASCADE"),
+                                         nullable=False)
     resultat: Mapped[str]
     scoreTeam1: Mapped[int]
     scoreTeam2: Mapped[int]
-    win: Mapped[int]
-    autres: Mapped[str]
-    datePrediction: Mapped[date] 
+    win: Mapped[int | None]
+    autres: Mapped[str | None]
+    datePrediction: Mapped[date | None] 
 
     __table_args__ = (
-        ForeignKeyConstraint(
-            ["equipe1", "equipe2", "stadeCompet"],
-            ["match.equipe1", "match.equipe2", "match.stadeCompet"], 
-            ondelete="CASCADE", 
-            onupdate="CASCADE",
-        ),
-
-        UniqueConstraint("individu_nomComplet", "equipe1", "equipe2", "stadeCompet"),
+        UniqueConstraint("individu_id", "idMatch"),
     )
+
     individu: Mapped["Individu"] = relationship(back_populates="predictions")
     game: Mapped["Match"] = relationship(back_populates="predictions") 
 
